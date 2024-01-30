65c777f7a70ec17d0433971cd77ac0de103301b8
Armando <borelliarmando28@gmail.com>
2024-01-30 12:24:58 -0500

    Added interface fn to collect command line arguments

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::net::IpAddr;
+
+fn interface() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 4 {
+        eprintln!("Not enough arguments provided. Usage: scanner -ip <IP_ADDRESS> -port <PORT>");
+        std::process::exit(1);
+    }
+
+    let ip_arg_index = args.iter().position(|arg| arg == "-ip").expect("Missing -ip argument");
+    let ip: IpAddr = args[ip_arg_index + 1].parse().expect("Invalid IP address");
+
+    let port_arg_index = args.iter().position(|arg| arg == "-port").expect("Missing -port argument");
+    let port: u16 = args[port_arg_index + 1].parse().expect("Invalid port number");
+
+    println!("Scanning Port: {} of IP: {}", port, ip);
+}
+
 fn main() {
-    println!("Hello, world!");
+    interface();
 }
