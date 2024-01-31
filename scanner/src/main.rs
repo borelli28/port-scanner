9cfcc79f42a2af15e05cf80807e38c05a8cc0d50
Armando <borelliarmando28@gmail.com>
2024-01-31 18:11:12 -0500

    Added parse_port_range to handle a range of ports

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
-use std::net::IpAddr;
-
+use std::net::{IpAddr};
 
 struct ScanArgs {
     ip: IpAddr,
@@ -10,20 +9,35 @@ struct ScanArgs {
 fn interface() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 4 {
-        eprintln!("Not enough arguments provided. Usage: scanner -ip <IP_ADDRESS> -port <PORT>");
+    if args.len() < 5 {
+        eprintln!("Not enough arguments provided. Usage: scanner -ip <IP_ADDRESS> -port <PORT_RANGE>");
         std::process::exit(1);
     }
 
     let ip_arg_index = args.iter().position(|arg| arg == "-ip").expect("Missing -ip argument");
-    let ip: IpAddr = args[ip_arg_index + 1].parse().expect("Invalid IP address");
+    let ip_str = &args[ip_arg_index + 1];
+    let ip: IpAddr = ip_str.parse().expect("Invalid IP address");
 
     let port_arg_index = args.iter().position(|arg| arg == "-port").expect("Missing -port argument");
-    let port: u16 = args[port_arg_index + 1].parse().expect("Invalid port number");
+    let port_range_str = &args[port_arg_index + 1];
+    let ports: Vec<u16> = parse_port_range(port_range_str).expect("Invalid port range");
 
-    ScanArgs {ip, port};
+    let scan_args = ScanArgs {
+        ip,
+        ports,
+    };
 
-    println!("Scanning Port: {} of IP: {}", port, ip);
+    println!("Scanning Ports: {:?} of IP: {}", scan_args.ports, scan_args.ip);
+}
+
+fn parse_port_range(range: &str) -> Result<Vec<u16>, Box<dyn std::error::Error>> {
+    let parts: Vec<&str> = range.split('-').collect();
+    if parts.len() != 2 {
+        return Err("Invalid port range format. Please use <START_PORT>-<END_PORT>".into());
+    }
+    let start: u16 = parts[0].parse()?;
+    let end: u16 = parts[1].parse()?;
+    Ok((start..=end).collect())
 }
 
 fn main() {
